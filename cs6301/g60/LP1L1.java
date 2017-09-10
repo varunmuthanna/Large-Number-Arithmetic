49f8c11a25ebf303a594e297f923e2904b37dcbb
shivan10 <31460456+shivan10@users.noreply.github.com>
2017-09-09 22:57:00 -0500

    added helper method: convertDeciamlToBase

--- cs6301/g60/LP1L1.java
+++ cs6301/g60/LP1L1.java
@@ -3,14 +3,22 @@
 // Change following line to your group number
 package cs6301.g60;
 
+import java.math.BigInteger;
+
 public class LP1L1 {
-    public static void main(String[] args) {
-	Num x = new Num(999);
-	Num y = new Num("8");
-	Num z = Num.add(x, y);
-	System.out.println(z);
-	Num a = Num.power(x, 8);
-	System.out.println(a);
-	z.printList();
+    public static void main(String[] args) throws Exception {
+        /*Num x = new Num(999);
+        Num y = new Num("8");
+        Num z = Num.add(x, y);
+        System.out.println(z);
+        Num a = Num.power(x, 8);
+        System.out.println(a);
+        z.printList();*/
+        /*Num n = new Num(9223372036854775807L);
+        n.printList();*/
+
+
+        Num x = new Num("9223372036854775807");
+        //System.out.println(Num.convertFromDecimalToBase(123123L,2));
     }
 }
